2bde2684572656fe1d1716e163ecb22cca96b01f
Alex Dergachev <alex@evolvingweb.ca>
2014-04-29 17:46:43 -0400

    minor code improvement

--- src/bg/background.js
+++ src/bg/background.js
@@ -6,30 +6,16 @@ var settings = new Store("settings", {
     "googleApiKey": "AIzaSyAICISSmAHfsclKJ4eu5UtbhhtWMLUqxcY"
 });
 
-/*
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete') {
-        // Execute some script when the page is fully (DOM) ready
-        chrome.tabs.executeScript(null, {code:"console.log('hiiii');"});
-    }
-});
-
-*/
-
-
 chrome.browserAction.onClicked.addListener(function(tab) {
   var files = ["src/inject/jquery.min.js", "src/inject/rangy-core.js","src/inject/gotgreek.js"];
   for (var i = 0; i < files.length; i++) {
     chrome.tabs.executeScript(tab.id, {file: files[i]});
   }
   chrome.tabs.executeScript(tab.id, {file: "src/inject/inject.js"}, function(){
-    chrome.tabs.sendMessage(tab.id, {srcLang: settings.get('srcLang'), targetLang: settings.get('targetLang')});
+    chrome.tabs.sendMessage(tab.id, {
+      msgId: "bootBabelFrog",
+      srcLang: settings.get('srcLang'),
+      targetLang: settings.get('targetLang')
+    });
   });
 });
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,4 +1,9 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  // flag to designate type of message
+  if (request.msgId != 'bootBabelFrog') {
+    return;
+  }
+
   jQuery(function($){
     gotGreek.boot(request.srcLang, request.targetLang);
   });
