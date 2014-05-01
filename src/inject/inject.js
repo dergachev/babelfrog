93e3865b56515cfc6d8bd2e519a09e2454876e9e
Alex Dergachev <alex@evolvingweb.ca>
2014-05-01 12:15:35 -0400

    Added a few code comments

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -4,15 +4,22 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     return;
   }
 
-
+  /**
+   * Override standardSuccesCallback to also play audio.
+   *
+   **/
   var extensionSuccessCallback = function(translation) {
-    // gotGreek.showMessage(translation, x, y);
     gotGreek.callbacks.standardSuccessCallback(translation);
     if (request.vocalize) {
+      // vocalize must be run from background.js
       chrome.runtime.sendMessage({msgId: "vocalize", text: gotGreek.currentJob.text});
     }
   }
 
+  /**
+   * Run BabelFish.
+   *
+   **/
   jQuery(function($){
     gotGreek.boot({
       source: request.srcLang,
