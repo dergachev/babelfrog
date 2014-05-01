8ef1ca4dc97c1363343e8d3feb253b4c7637f7c0
Alex Dergachev <alex@evolvingweb.ca>
2014-05-01 12:16:02 -0400

    Minor code cleanup

--- src/options_custom/settings.js
+++ src/options_custom/settings.js
@@ -1,11 +1,3 @@
 window.addEvent("domready", function () {
-    new FancySettings.initWithManifest(function (settings) {
-      /*
-        settings.manifest.vocalize.addEvent("change", function (){
-          console.log(this);
-          console.log("changed You clicked me!");
-        });
-      console.log(settings);
-      */
-    });
+    new FancySettings.initWithManifest();
 });
